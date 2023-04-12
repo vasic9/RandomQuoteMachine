02a4badce65cb6f730526ad2826296493b0f7e31
Nemanja <57727863+vasic9@users.noreply.github.com>
2023-04-12 19:09:50 +0200

    Added bootstrap, background

---
+++ quotes.js
@@ -0,0 +1,77 @@
+const QUOTESTASH = [
+    {
+        quote: "The right man in the wrong place can make all the difference in the world.",
+        author: "-Half-Life 2",
+        backgroundimg: "url(https://images4.alphacoders.com/570/570284.jpg)"
+    },
+    {
+        quote: "Nothing is true, everything is permitted.",
+        author: "-Assassin's Creed",
+        backgroundimg: "url(https://w.wallhaven.cc/full/45/wallhaven-458jr5.jpg)"
+    },
+    {
+        quote: "War is where the young and stupid are tricked by the old and bitter into killing each other.",
+        author: "-Grand Theft Auto IV",
+        backgroundimg: "url(https://w.wallhaven.cc/full/nm/wallhaven-nm9zp9.jpg)"
+    },
+    {
+        quote: "Hope is what makes us strong. It is why we are here. It is what we fight with when all else is lost.",
+        author: "-God of War 3",
+        backgroundimg: "url(https://w.wallhaven.cc/full/ox/wallhaven-oxz269.jpg)"
+    },
+    {
+        quote: "A man chooses; a slave obeys.",
+        author: "-Bioshock",
+        backgroundimg: "url(https://w.wallhaven.cc/full/lm/wallhaven-lmw66y.jpg)"
+    },
+    {
+        quote: "What is a drop of rain, compared to the storm? What is a thought, compared to the mind?",
+        author: "-System Shock 2",
+        backgroundimg: "url(https://w.wallhaven.cc/full/95/wallhaven-9566q8.jpg)"
+    },
+    {
+        quote: "No matter how dark the night, the morning always comes.",
+        author: "-Final Fantasy X",
+        backgroundimg: "url(https://w.wallhaven.cc/full/g7/wallhaven-g7qe13.png)"
+    },
+    {
+        quote: "What is bravery, without a dash of recklessness?",
+        author: "-Dark Souls",
+        backgroundimg: "url(https://w.wallhaven.cc/full/zy/wallhaven-zymlzj.png)"
+    },
+    {
+        quote: "A hero need not speak. When he is gone, the world will speak for him",
+        author: "-Halo",
+        backgroundimg: "url(https://w.wallhaven.cc/full/0p/wallhaven-0pz92e.jpg)"
+    },
+    {
+        quote: "What is better - to be born good, or to overcome your evil nature through great effort?",
+        author: "-The Elder Scrolls V: Skyrim",
+        backgroundimg: "url(https://w.wallhaven.cc/full/p2/wallhaven-p2d8w9.jpg)"
+    },
+    {
+        quote: "There are two ways of arguing with a woman, and neither one works.",
+        author: "-Red Dead Redemption",
+        backgroundimg: "url(https://w.wallhaven.cc/full/5d/wallhaven-5dkgp3.jpg)"
+    },
+    {
+        quote: "We all make choices, but in the end, choices make us.",
+        author: "-Bioshok",
+        backgroundimg: "url(https://w.wallhaven.cc/full/lm/wallhaven-lmw66y.jpg)"
+    },
+    {
+        quote: "Working for better tomorrow isn't just one-time thing. The tomorrows keep coming.",
+        author: "-Yu Narukami",
+        backgroundimg: "url(https://w.wallhaven.cc/full/76/wallhaven-767m9o.png)"
+    },
+    {
+        quote: "In the darkes hour, there is always a way out.",
+        author: "-Eve, Mass Effect 3",
+        backgroundimg: "url(https://w.wallhaven.cc/full/vm/wallhaven-vmr1g8.png)"
+    },
+    {
+        quote: "Pressure turns to power, when God does the pressing.",
+        author: "-T.D. Jakes",
+        backgroundimg: "url(https://w.wallhaven.cc/full/nz/wallhaven-nz8gxj.jpg)"
+    },
+]
