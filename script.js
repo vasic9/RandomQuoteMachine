7dfd5b4cf9984f74e352f4cd41bf56d6fd177c30
Nemanja <57727863+vasic9@users.noreply.github.com>
2023-04-13 17:42:37 +0200

    Add random quotes generator

--- script.js
+++ script.js
@@ -1,64 +1,18 @@
-const quoteStash = [
-    {
-        quote: "People who fit, do not seek.",
-        author: "-"
-    },
-    {
-        quote: "The right man in the wrong place can make all the difference in the world.",
-        author: "-Half-Life 2"
-    },
-    {
-        quote: "The world fears the inevitable plummet into the abyss. Watch for that moment and when it comes, do not hesitate to leap. It is only when you fall that you learn whether you can fly.",
-        author: "-Dragon Age: Origins"
-    },
-    {
-        quote: "Nothing is true, everything is permitted.",
-        author: "-Assassin's Creed"
-    },
-    {
-        quote: "War is where the young and stupid are tricked by the old and bitter into killing each other.",
-        author: "-Grand Theft Auto IV"
-    },
-    {
-        quote: "Hope is what makes us strong. It is why we are here. It is what we fight with when all else is lost.",
-        author: "-God of War 3"
-    },
-    {
-        quote: "A man chooses; a slave obeys.",
-        author: "-Bioshock"
-    },
-    {
-        quote: "What is a drop of rain, compared to the storm? What is a thought, compared to the mind?",
-        author: "-System Shock 2"
-    },
-    {
-        quote: "No matter how dark the night, the morning always comes.",
-        author: "-Final Fantasy X"
-    },
-    {
-        quote: "What is bravery, without a dash of recklessness?",
-        author: "-Dark Souls"
-    },
-    {
-        quote: "A hero need not speak. When he is gone, the world will speak for him",
-        author: "-Halo"
-    },
-    {
-        quote: "What is better - to be born good, or to overcome your evil nature through great effort?",
-        author: "-The Elder Scrolls V: Skyrim"
-    },
-    {
-        quote: "There are two ways of arguing with a woman, and neither one works.",
-        author: "Red Dead Redemption"
-    }
-]
 window.onload = function() {
     newQuote();
 };
 
 let newQuote = () => {
-    let arrQuote = Math.floor(Math.random() * quoteStash.length);
-    document.getElementById("text").innerText = quoteStash[arrQuote].quote;
-    document.getElementById("author").innerText = quoteStash[arrQuote].author;
-    console.log("newquote");
+    let quoteSize = QUOTESTASH.length;
+    let arrQuote = Math.floor(Math.random() * quoteSize);
+    let randomQuote = QUOTESTASH[arrQuote];
+
+    let twQuote = "https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text="
+    twQuote += "How%20wonderful%20it%20is%20that%20nobody%20need%20wait%20a%20single%20moment%20before%20starting%20to%20improve%20the%20world.%22%20"
+    twQuote += "Anne%20Frank"
+
+    document.getElementById("tweet-quote").href = twQuote;
+    document.getElementById("text").innerText = randomQuote.quote;
+    document.getElementById("author").innerText = randomQuote.author;
+    document.getElementById("backgroud-pic").style.backgroundImage = randomQuote.backgroundimg;
 }
